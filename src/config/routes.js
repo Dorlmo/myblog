80e73f76c21d14905e431e1b17d64663682eedd4
Dorlmo <86467190+Dorlmo@users.noreply.github.com>
2023-07-16 17:09:30 +0800

    bug fixed

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,7 +2,6 @@ export default [
     { path: '/', component: () => import('../pages/Home.vue') },
     { path: '/home', component: () => import('../pages/Home.vue') },
     { path: '/index', component: () => import('../pages/Index.vue') },
-    { path: '/search', component: () => import('../pages/Search.vue') },
     { path: '/about', component: () => import('../pages/About.vue') },
     { path: '/test', component: () => import('../pages/Test.vue') },
     { path: '/content', component: () => import('../pages/Content.vue') },
