96d3ee9762c3a66f115e228bc59f37ee1483ee12
Dorlmo <86467190+Dorlmo@users.noreply.github.com>
2023-07-17 14:14:03 +0800

    bug fixed

--- src/util/format.ts
+++ src/util/format.ts
@@ -1,3 +1,4 @@
+
 /**
  * @description 处理.md字符串，使其中的图片和链接的路径适配于浏览器
  */
@@ -6,7 +7,7 @@ export const processMarkdownPaths = (markdownText: string) => {
     markdownText = markdownText.replace(/\\/g, "/");
     const linkRegex = /\[(.*?)\]\((.*)\)/g;
     markdownText = markdownText.replace(linkRegex, (_match, altText, linkPath) => {
-        const processedLinkPath = linkPath.replace(/.*(?=\/content\/)/,'');
+        const processedLinkPath = linkPath.replace(/.*(?=\/content\/)/,'.');
         return `[${altText}](${processedLinkPath})`;
     });
 
