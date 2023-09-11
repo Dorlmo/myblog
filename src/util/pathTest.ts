3bc26ee250427d8abab359509b9fe1d2d7d6e86e
Dorlmo <86467190+Dorlmo@users.noreply.github.com>
2023-09-11 23:24:13 +0800

    added:Content update #18

--- src/util/pathTest.ts
+++ src/util/pathTest.ts
@@ -1,12 +1,13 @@
 import fs from 'fs';
 
 export const isURL = (path: string): boolean => {
-    try {
-        new URL(path);
-        return true;
-    } catch (_err) {
-        return false;
-    }
+    var urlPattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+    return !!urlPattern.test(path);
 }
 
 export const fileReachable = (path: string): boolean => {
