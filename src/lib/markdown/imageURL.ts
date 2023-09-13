c0e1df08d88d73a079b45b5d0b39075d6e6de5b9
Dorlmo <86467190+Dorlmo@users.noreply.github.com>
2023-09-13 22:52:09 +0800

    fixed:Bug fixed #22 added:Content update #18

--- src/lib/markdown/imageURL.ts
+++ src/lib/markdown/imageURL.ts
@@ -1,16 +1,16 @@
 import MarkdownIt from 'markdown-it';
-import { join, dirname, resolve, relative, isAbsolute } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { isURL } from '../../util/pathTest.ts';
 import { baseURL } from '../../../vite.config.ts';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const BLOG_FOLDER_PATH = resolve(__dirname, '../../../content');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const BLOG_FOLDER_PATH = path.resolve(__dirname, '../../../content');
 
 export const imageURL = (md: MarkdownIt) => {
   md.renderer.rules.image = function (tokens, idx, _options, env, _self) {
     const token = tokens[idx];
-    const src: string | null = token.attrGet('src');
+    const src: string | undefined = token.attrGet('src')?.normalize();
     const alt: string = token.content;
 
     if (!src || !env.path) {
@@ -19,18 +19,18 @@ export const imageURL = (md: MarkdownIt) => {
 
     //如果是链接，则直接返回，如果是相对路径，则转化为绝对路径
     let newPath: string = decodeURIComponent(src);
-    if (!isAbsolute(newPath)) {
+    if (!path.isAbsolute(newPath)) {
       if (isURL(newPath)) {
         return `<img class="lazyload" data-src="${newPath}" alt="${alt}" />`;
       } else {
-        newPath = join(dirname(env.path), newPath);
+        newPath = path.join(path.dirname(env.path), newPath);
       }
     }
 
     //将绝对路径进行下一步处理变成目标字符串
-    newPath = relative(BLOG_FOLDER_PATH, newPath);
-    if (!newPath.startsWith('..') && !isAbsolute(newPath)) {
-      newPath = join(baseURL, 'content', newPath).replace(/\\/g, "/");
+    newPath = path.relative(BLOG_FOLDER_PATH, newPath);
+    if (!newPath.startsWith('..') && !path.isAbsolute(newPath)) {
+      newPath = path.join(baseURL, 'content', newPath);
       return `<img class="lazyload" data-src="${newPath}" alt="${alt}" />`;
     } else {
       console.log(`File: ${src} from ${env.path} is not in the content folder`);
